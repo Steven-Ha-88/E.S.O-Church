048e0731fb09a3164db8074231b4c4add61e730f
Steven Ha <ming@Stevens-MacBook-Pro.local>
2020-12-09 20:07:05 +0000

    serve static assets

--- server.js
+++ server.js
@@ -27,6 +27,16 @@ const songsRouter = require("./routes/songs");
 // app uses endpoints for following url and its routes
 app.use("/songs", songsRouter);
 
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
