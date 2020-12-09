a6d042342e4851a67b6e8a429d9394e490050a41
Steven Ha <ming@Stevens-MacBook-Pro.local>
2020-12-09 21:02:32 +0000

    import path error

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const path = require("path");
 
 // Dotenv loads environment variables from a .env file into process.env.
 require("dotenv").config();
